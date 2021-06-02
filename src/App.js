64dd402ccffa023db3039ce8f8a9d9edc1e99c0c
dtd3103 <dtd300996@gmail.com>
2021-06-02 20:48:55 +0700

    cra

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import CountySelector from "./components/CountySelector";
+import Highlight from "./components/Highlight";
+import Sumary from "./components/Sumary";
+
+function App() {
+  return (
+    <>
+      <CountySelector />
+      <Highlight />
+      <Sumary />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sumary/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sumary(props) {
+  return (
+    <div>
+      3
+    </div>
+  );
+}
+
+export default Sumary;
